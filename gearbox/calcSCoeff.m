6d99408a2bf93af99478bf9c80292612eadabd8e
mihagazvoda <miha.gazvoda@student.fmf.uni-lj.si>
2015-07-21 09:19:33 +0200

    updated version

--- gearbox/calcSCoeff.m
+++ gearbox/calcSCoeff.m
@@ -13,63 +13,61 @@ function [s_coefficients] = calcSCoeff(M)
     sum_E_l = 0;
     sum_E_in = 0;
 
-    for i = 1:length(M) - 1
-        t = (M(i,1) + M(i+1,1)) / 2;
-        dt = M(i+1,1) - M(i,1);
-%         T = (M(i+1,7) + M(i,7)) / 2;
-%         dT = M(i+1,7) - M(i,7);
-%         rpm = (M(i,9) + M(i+1,9)) / (2 * 60000);
-%         torque = (M(i,11) + M(i+1,11)) * 1000 / 2;
-%         T_a = (M(i,10) + M(i+1,10)) / 2;
+    for i = 2:length(M)
+        t = M(i,1);
+        dt = M(i, 1) - M(i-1,1);
         T = M(i,7);
-        dT = M(i+1,7) - M(i,7);
+        dT = M(i,7) - M(i-1,7);
         rpm = M(i,9) / 60000;
         torque = M(i,11) * 1000;
         T_a = M(i,10);
         
 
-            if rpm*torque > 7 %RPM*torque>1000 (units from measurments)
-                %avg or not?
-                %vmesno racunanje?
-                P_in = 2 * pi * torque * rpm;
-                E_l = (T - T_a - a) * (1 - exp(-L*dt));
-                
-                sum_dT = sum_dT + dT;
-                sum_E_l = sum_E_l + E_l;
-                sum_E_in = sum_E_in + P_in * dt;
-                
+        if rpm*torque >= 7 %RPM*torque>1000 (units from measurments)
+            %avg or not?
+            %vmesno racunanje?
+            P_in = 2 * pi * torque * rpm;
+            E_l = (T - T_a - a) * (1 - exp(-L*dt));
 
-                if isDrilling == false 
-                    start = t;
-                    start_T = T;
-                    isDrilling = true;
-                end
-                
-            else
-                stop = t;
-                stop_T = T;
-                if ((stop - start) > 2000000) && (start > 0)
-                    j = j + 1;
-                    c = (sum_dT + sum_E_l) / sum_E_in;
-                    avg_T = (start_T + stop_T)/2; 
-                    s_coefficients(j,1) = c;
-                    s_coefficients(j,2) = avg_T;
-                    s_coefficients(j,3) = stop-start;
-                    s_coefficients(j,4) = start;
-                    s_coefficients(j,5) = stop;
-                    
-                    check_c(c,avg_T,start,stop)
+            sum_dT = sum_dT + dT;
+            sum_E_l = sum_E_l + E_l;
+            sum_E_in = sum_E_in + P_in * dt;
 
-                end
-                
-                start = 0;
-                stop = 0;
-                start_T = 0;
-                stop_T = 0;
-                isDrilling = false;
-                sum_dT = 0;
-                sum_E_l = 0;
-                sum_E_in = 0;
+            c = (sum_dT + sum_E_l) / sum_E_in;
+            
+            if mod(i, 10000) == 0
+                disp(['c = ',num2str(c)]);
             end
+
+            if ~isDrilling 
+                start = t;
+                start_T = T;
+                isDrilling = true;
+            end
+        else
+            stop = t;
+            stop_T = T;
+            if start > 0 && stop - start >= 5*60*1000
+                j = j + 1;
+                c = (sum_dT + sum_E_l) / sum_E_in;
+                avg_T = (start_T + stop_T)/2; 
+                s_coefficients(j,1) = c;
+                s_coefficients(j,2) = avg_T;
+                s_coefficients(j,3) = stop-start;
+                s_coefficients(j,4) = start;
+                s_coefficients(j,5) = stop;
+
+                check_c(c,avg_T,start,stop)
+            end
+
+            start = 0;
+            stop = 0;
+            start_T = 0;
+            stop_T = 0;
+            isDrilling = false;
+            sum_dT = 0;
+            sum_E_l = 0;
+            sum_E_in = 0;
+        end
     end
 end
